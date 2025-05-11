3f1c206f1e276cd522746ac56387bb4f8b74464f
mangert <mangert@yandex.ru>
2025-05-11 18:52:24 +0300

    config updated

--- scripts/config.ts
+++ scripts/config.ts
@@ -6,20 +6,20 @@
     {
         token_name:  "GreekAlphabet", 
         token_sym: "ELL",
-        link: "https://raw.githubusercontent.com/mangert/NFTMetadataStorage/refs/heads/main/",
-        max_supply: 25n, //maxSupply
-        mint_price: 100000000000000000n       
+        link: "https://raw.githubusercontent.com/mangert/NFTMetadataStorage/refs/heads/main/",        
+        mint_price: 100000000000000000n,
+        max_supply: 25n //maxSupply       
 
     }
     const getERC721ArgsArray = (): [string, string, string, bigint, bigint] => [
         ERC721_argsObject.token_name,
         ERC721_argsObject.token_sym,
-        ERC721_argsObject.link,
-        ERC721_argsObject.max_supply,
-        ERC721_argsObject.mint_price
+        ERC721_argsObject.link,        
+        ERC721_argsObject.mint_price,
+        ERC721_argsObject.max_supply
 ];
         
-    const ERC721_contractAddress = "0xA6B8dac4980b4C8Bc46ff8a28e256699a337fD26"; //подставить после деплоя
+    const ERC721_contractAddress = "0xE1Cc4321864daB4F1Ab8d02A42eafFa8A56236c1"; //подставить после деплоя
 
 
 export {ERC20_contractAddress, getERC721ArgsArray, ERC721_contractAddress};
